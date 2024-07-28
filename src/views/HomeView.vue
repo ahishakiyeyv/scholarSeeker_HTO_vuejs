f7e0f95cf3aff291269d044ac9c0ef9999002ee5
ahishakiyeyv <yvanahishakiye6@gmail.com>
2024-07-28 21:41:04 +0100

    homepage updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,7 +30,33 @@ export default {
 </script>
 <style scoped>
 .section1{
-  padding:9.5rem 4rem 8rem 8rem;
-  width:25rem;
+  padding:11.5rem 4rem 8rem 8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border:1px solid red;
+}
+.leftSide{
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  width:30rem;
+  border:1px solid red;
+}
+.leftSide h1{
+  font-size: 2rem;
+}
+.leftSide p{
+  font-size: 1rem;
+  line-height: 1.5rem;
+}
+.rightSide img{
+  height:100px;
+}
+.actionBtn{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
